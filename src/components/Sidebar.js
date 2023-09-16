8c022822c82c7e2367da49884cde24d3ea234d6c
joshua <joshuadavidoni@yahoo.com>
2023-09-16 17:11:40 +0100

    2023 fixes

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -4,31 +4,31 @@ import { faHome, faPlusCircle, faCommenting, faMedkit } from '@fortawesome/free-
 import { faCanadianMapleLeaf } from '@fortawesome/free-brands-svg-icons'
 
 const Sidebar = () => {
-    return (  
-        <div className='sidebar card'>
-            <div className='cog'>
-                <FontAwesomeIcon  icon={faCanadianMapleLeaf} size="2x" />
-            </div>
-            <div>
-                <div className='item'>
-                    <FontAwesomeIcon icon={faHome} size="lg" />
-                    <h6>Dashboard</h6>
-                </div>
-                <div  className='item plus'>
-                    <FontAwesomeIcon className='dr' icon={faPlusCircle} size="lg" />
-                    <h6>Doctor</h6>
-                </div>
-                <div className='item'>
-                    <FontAwesomeIcon icon={faCommenting} size="lg" />
-                    <h6>Contact</h6>
-                </div>
-                <div className='item'>
-                    <FontAwesomeIcon icon={faMedkit} size="lg" />
-                    <h6>Medicine</h6>
-                </div>
-            </div>
+  return (
+    <div className='sidebar'>
+      <div className='cog'>
+        <FontAwesomeIcon icon={faCanadianMapleLeaf} size="2x" />
+      </div>
+      <div>
+        <div className='item'>
+          <FontAwesomeIcon icon={faHome} size="lg" />
+          <h6>Dashboard</h6>
         </div>
-    );
+        <div className='item plus'>
+          <FontAwesomeIcon className='dr' icon={faPlusCircle} size="lg" />
+          <h6>Doctor</h6>
+        </div>
+        <div className='item'>
+          <FontAwesomeIcon icon={faCommenting} size="lg" />
+          <h6>Contact</h6>
+        </div>
+        <div className='item'>
+          <FontAwesomeIcon icon={faMedkit} size="lg" />
+          <h6>Medicine</h6>
+        </div>
+      </div>
+    </div>
+  );
 }
- 
+
 export default Sidebar;
